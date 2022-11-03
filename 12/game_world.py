3c86ffe8b2551247330c2de837eb6e5363052f0f
psb2199 <psb2199@naver.com>
2022-11-03 15:39:08 +0900

    DRILL 12번 완료
    2019182016 박수빈

---
+++ 12/game_world.py
@@ -0,0 +1,27 @@
+# game world
+
+world = [[],[]]
+
+def add_object(o,depth):
+    world[depth].append(o)
+
+def add_objects(ol, depth):
+    world[depth] += ol
+
+def all_objects():
+    for layer in world:
+        for o in layer:
+            yield o
+def clear():
+    for o in all_objects():
+        del o
+    for layer in world:
+        layer.clear()
+
+def remove_object(o):
+    for layer in world:
+        if o in layer:
+            layer.remove(o)
+            del o
+            return
+    raise ValueError('Trying destroy non existing object')
